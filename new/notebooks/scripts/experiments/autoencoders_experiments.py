f6eaf045d2ffe313e0e4993bbbff9b051c400d00
Csanad <csanad00058@gmail.com>
2025-04-17 22:33:35 +0200

    Added file for running multiple experiments with normal autoencoders

---
+++ new/notebooks/scripts/experiments/autoencoders_experiments.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import sys
+sys.path.append("../..")
+from modules.autoencoders import *
+from modules.modelwithtrainer import *
+
+
+# Loading data
+inputFeature = pd.read_csv('../../../Data/NIBRS_ND_2021/processed/input.csv', index_col='Unnamed: 0')
+# Separating numerical and categorical features
+numerical_features=['population','victim_seq_num','age_num_victim','incident_hour','incident_month','incident_day','incident_dayofmonth','incident_weekofyear']
+categorical_features = ['resident_status_code','race_desc_victim',
+'ethnicity_name_victim','pub_agency_name','offense_name','location_name','weapon_name'
+,'injury_name','relationship_name','incident_isweekend']
+# Onehot-encoding categorical features
+inputFeature_1h = pd.get_dummies(inputFeature, columns=categorical_features)
+
+
+categorymapper = {
+    "all_columns" : categorical_features,
+    "all_indices" : [],
+    "columns": {},
+    "values": {}
+}
+
+for column in inputFeature_1h.columns:
+    splitted_colname = column.split(" ")
+    double_splitted_colname = splitted_colname[0].split("_")
+    original_colname = "_".join(double_splitted_colname[:-1])
+    if original_colname in categorical_features:
+        first_value = double_splitted_colname[-1:]
+        column_value = " ".join(first_value + splitted_colname[1:])
+        column_index = inputFeature_1h.columns.get_loc(column)
+        categorymapper["all_indices"].append(column_index)
+        categorymapper["columns"].update({column_index:original_colname})
+        categorymapper["values"].update({column_index:column_value})
+
+
+# Convert object columns to numeric if they represent categories
+for column in inputFeature_1h.select_dtypes(include=['object']):
+    inputFeature_1h[column] = inputFeature_1h[column].astype('category').cat.codes
+
+# Train-test split
+train, test = train_test_split(inputFeature_1h, test_size=0.1, random_state=42)
+
+# Scaling features
+scaler = MinMaxScaler().fit(train[numerical_features])
+train[numerical_features] = scaler.transform(train[numerical_features])
+test[numerical_features] = scaler.transform(test[numerical_features])
+
+# Converting data to tensors
+X_train = torch.nan_to_num(torch.Tensor(train.values.astype(np.float32)))
+y_train = torch.nan_to_num(torch.Tensor(train.values.astype(np.float32)))
+
+X_test = torch.nan_to_num(torch.Tensor(test.values.astype(np.float32)))
+y_test = torch.nan_to_num(torch.Tensor(test.values.astype(np.float32)))
+
+
